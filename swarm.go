c8f2bbf3d849b02cc8544dce83a0f9823b7e6767
Vitaly Haritonsky <vharitonsky@gmail.com>
2014-09-18 12:52:25 +0300

    added worker reports, worker routines moved to struct to hold state

--- swarm.go
+++ swarm.go
@@ -2,8 +2,11 @@ package swarm
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/fzzy/radix/redis"
 	"log"
+	"math"
+	"os"
 	"runtime"
 	"time"
 )
@@ -17,10 +20,27 @@ type Master struct {
 	queue string
 }
 
+type Slave struct {
+	id           string
+	addr         string
+	queue        string
+	pool         chan string
+	workersCount int
+}
+
 type Job struct {
 	Name string
 	Args []interface{}
 	TTL  int
+}
+
+type SlaveReport struct {
+	Id           string
+	BusyWorkers  int
+	TotalWorkers int
+	Hostname     string
+	ListeningTo  string
+	Generated    string
 }
 
 type Handler interface {
@@ -37,19 +57,107 @@ func NewMaster(addr, queue string) *Master {
 	return &Master{addr: addr, queue: queue}
 }
 
+func NewSlave(addr, queue string, workersCount int) *Slave {
+	slave := Slave{
+		id:           GenerateId(),
+		addr:         addr,
+		queue:        queue,
+		workersCount: workersCount,
+		pool:         make(chan string, workersCount),
+	}
+	for i := 0; i < workersCount; i++ {
+		slave.pool <- GenerateId()
+	}
+	return &slave
+}
+
+func (s *Slave) Serve() (err error) {
+	redisConn, err := redis.Dial("tcp", s.addr)
+	if err != nil {
+		err = fmt.Errorf("Can't connect to master@%s: %s", s.addr, err)
+		return
+	}
+	defer redisConn.Close()
+	go func() {
+		for {
+			s.SubmitReport()
+			time.Sleep(30 * time.Second)
+		}
+	}()
+	for workerId := range s.pool {
+		job := Job{}
+		reply, err := redisConn.Cmd("BRPOP", s.queue, 0).List()
+		if err != nil {
+			return fmt.Errorf("Can't receive job message:", err)
+		}
+		err = json.Unmarshal([]byte(reply[1]), &job)
+		if err != nil {
+			log.Printf("Can't parse job message:", err)
+			s.pool <- workerId
+			continue
+		}
+		handler, found := handlerMap[job.Name]
+		if !found {
+			log.Printf("Cannot process job %s", job.Name)
+			job.TTL -= 1
+			if job.TTL > 0 {
+				jobBlob, _ := json.Marshal(job)
+				redisConn.Cmd("PUSH", s.queue, string(jobBlob))
+			}
+			s.pool <- workerId
+			continue
+		}
+		go func(workerId string, job Job) {
+			log.Printf("[%s] Received job '%s' with args '%v'", workerId, job.Name, job.Args)
+			start_time := time.Now()
+			handler.Handle(job.Args...)
+			log.Printf("[%s] Job '%s' with args '%v' complete in %s", workerId, job.Name, job.Args, time.Since(start_time))
+			s.pool <- workerId
+		}(workerId, job)
+	}
+	return nil
+}
+
+func (s *Slave) GetReport() SlaveReport {
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatalf("Can't get hostname: %s", err)
+	}
+	return SlaveReport{
+		Id:           s.id,
+		TotalWorkers: s.workersCount,
+		BusyWorkers:  s.workersCount - len(s.pool),
+		ListeningTo:  s.queue,
+		Hostname:     hostname,
+		Generated:    time.Now().Format(time.UnixDate),
+	}
+}
+
+func (s *Slave) SubmitReport() {
+	redisConn, err := redis.Dial("tcp", s.addr)
+	if err != nil {
+		err = fmt.Errorf("Can't connect to master to submit report@%s: %s", s.addr, err)
+		return
+	}
+	defer redisConn.Close()
+	report := s.GetReport()
+	slaveReport, err := json.Marshal(report)
+	redisConn.Cmd("HSET", "_swarm_worker_reports", s.id, string(slaveReport))
+}
+
 func (m *Master) Submit(name string, args ...interface{}) {
-	redis_conn, err := redis.Dial("tcp", m.addr)
+	redisConn, err := redis.Dial("tcp", m.addr)
 	if err != nil {
 		log.Fatalf("Can't connect to redis@%s: %s", m.addr, err)
 	}
-	defer redis_conn.Close()
+	defer redisConn.Close()
 	job := Job{
 		Name: name,
 		Args: args,
 		TTL:  0,
 	}
 	jobBlob, _ := json.Marshal(job)
-	redis_conn.Cmd("LPUSH", m.queue, string(jobBlob))
+	redisConn.Cmd("LPUSH", m.queue, string(jobBlob))
 }
 
 func Handle(jobName string, h Handler) {
@@ -59,50 +167,62 @@ func Handle(jobName string, h Handler) {
 	handlerMap[jobName] = h
 }
 
-func Listen(addr, queue string, workers int) {
-	redis_conn, err := redis.Dial("tcp", addr)
-	if err != nil {
-		log.Fatalf("Can't connect to master@%s: %s", addr, err)
-	}
-	defer redis_conn.Close()
-	freeWorkersChan := make(chan string, workers)
-	for i := 0; i < workers; i++ {
-		workerId := GenerateId()
-		freeWorkersChan <- workerId
-	}
+func (m *Master) WatchSlaves() {
+	for {
+		time.Sleep(30 * time.Second)
+		redisConn, err := redis.Dial("tcp", m.addr)
+		if err != nil {
+			log.Fatalf("Can't connect to master@%s: %s", m.addr, err)
+		}
+		defer redisConn.Close()
+		reply, err := redisConn.Cmd("HGETALL", "_swarm_worker_reports").List()
+		if err != nil {
+			log.Fatalf("Can't get reply from master@%s: %s", m.addr, err)
+		}
+		if len(reply) == 0 {
+			log.Print("No running slaves")
+			continue
+		}
+		var lastkey string
 
-	runtime.GOMAXPROCS(workers)
-	for workerId := range freeWorkersChan {
-		job := Job{}
-		reply, err := redis_conn.Cmd("BRPOP", queue, 0).List()
-		if err != nil {
-			log.Printf("Can't receive job message:", err)
-			freeWorkersChan <- workerId
-			continue
-		}
-		err = json.Unmarshal([]byte(reply[1]), &job)
-		if err != nil {
-			log.Printf("Can't parse job message:", err)
-			freeWorkersChan <- workerId
-			continue
-		}
-		handler, found := handlerMap[job.Name]
-		if !found {
-			log.Printf("Cannot process job %s", job.Name)
-			job.TTL -= 1
-			if job.TTL > 0 {
-				jobBlob, _ := json.Marshal(job)
-				redis_conn.Cmd("PUSH", queue, string(jobBlob))
+		runningReports := make([]string, 0)
+
+		for i, value := range reply {
+			if math.Mod(float64(i+1), 2) != 0 {
+				lastkey = value
+				continue
 			}
-			freeWorkersChan <- workerId
+			report := SlaveReport{}
+			err = json.Unmarshal([]byte(value), &report)
+			if err != nil {
+				log.Fatalf("Can't parse worker report: %s", err)
+			}
+			reportGenerationTime, err := time.Parse(time.UnixDate, report.Generated)
+			if err != nil || time.Since(reportGenerationTime) > 1*time.Minute {
+				redisConn.Cmd("HDEL", "_swarm_worker_reports", lastkey)
+				continue
+			}
+			runningReports = append(runningReports, fmt.Sprintf("\n" +
+				"ID: %s\n"+
+				"HOST: %s\n"+
+				"WORKERS: %d/%d\n"+
+				"QUEUES: %s\n"+
+				"LAST SEEN: %s\n\n",
+				report.Id, report.Hostname, report.TotalWorkers, report.BusyWorkers, report.ListeningTo, report.Generated))
+		}
+		if len(runningReports) == 0 {
+			log.Print("No running slaves")
 			continue
 		}
-		go func(workerId string, job Job) {
-			log.Printf("[%s] Received job '%s' with args '%v'", workerId, job.Name, job.Args)
-			start_time := time.Now()
-			handler.Handle(job.Args...)
-			log.Printf("[%s] Job '%s' with args '%v' complete in %s", workerId, job.Name, job.Args, time.Since(start_time))
-			freeWorkersChan <- workerId
-		}(workerId, job)
+		log.Print("====START SLAVE REPORT====")
+		for _, report := range runningReports {
+			fmt.Print(report)
+		}
+		log.Print("==== END SLAVE REPORT ====")
 	}
+}
+
+func Listen(addr, queue string, workersCount int) error {
+	runtime.GOMAXPROCS(workersCount)
+	return NewSlave(addr, queue, workersCount).Serve()
 }
